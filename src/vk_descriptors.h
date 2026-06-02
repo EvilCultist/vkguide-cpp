1cd25c029d6cf9516985f7a689c272082487d7ea
me <66961093+EvilCultist@users.noreply.github.com>
2026-06-02 19:02:31 +0000

    uploading current progress to github

---
+++ src/vk_descriptors.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include <cstdint>
+#include <span>
+#include <vector>
+#include <vk_types.h>
+#include <vulkan/vulkan_core.h>
+
+struct DescriptorLayputBuilder {
+  std::vector<VkDescriptorSetLayoutBinding> Bindings;
+
+  void add_binding(uint32_t binding, VkDescriptorType type);
+  void clear();
+
+  VkDescriptorSetLayout build(VkDevice device, VkShaderStageFlags shaderStages,
+                              void *pnext = nullptr,
+                              VkDescriptorSetLayoutCreateFlags flags = 0);
+};
+
+struct DescriptorAllocator {
+
+  struct PoolSizeRatio {
+    VkDescriptorType type;
+    float ratio;
+  };
+
+  VkDescriptorPool pool;
+
+  void init_pool(VkDevice device, uint32_t maxSets,
+                 std::span<PoolSizeRatio> poolRatios);
+  void clear_descriptors(VkDevice device);
+  void destroy_pool(VkDevice device);
+
+  VkDescriptorSet allocate(VkDevice device, VkDescriptorSetLayout layout);
+};
---
+++ src/vk_engine.h
@@ -0,0 +1,152 @@
+﻿// vulkan_guide.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <cstdint>
+#include <deque>
+#include <functional>
+#include <vector>
+#include <vk_types.h>
+#include <vulkan/vulkan_core.h>
+
+#include "VkBootstrap.h"
+#include "glm/ext/vector_float4.hpp"
+#include "vk_descriptors.h"
+
+struct ComputePushConstants {
+  glm::vec4 color1;
+  glm::vec4 color2;
+  glm::vec4 data3;
+  glm::vec4 data4;
+};
+
+struct ComputeEffect {
+  const char *name;
+
+  VkPipeline pipeline;
+  VkPipelineLayout layout;
+
+  ComputePushConstants data;
+};
+
+struct DeletionQueue {
+  std::deque<std::function<void()>> deletors;
+
+  void push_function(std::function<void()> &&function) {
+    deletors.push_back(function);
+  }
+
+  void flush() {
+    for (auto it = deletors.rbegin(); it != deletors.rend(); it++) {
+      (*it)();
+    }
+
+    deletors.clear();
+  }
+};
+
+struct FrameData {
+  VkCommandPool _commandPool;
+  VkCommandBuffer _mainCommandBuffer;
+
+  VkSemaphore _swapchainSemaphore, _renderSemaphore;
+  VkFence _renderFence;
+  DeletionQueue _deletionQueue;
+};
+
+struct AllocatedImage {
+  VkImage image;
+  VkImageView imageView;
+  VmaAllocation allocation;
+  VkExtent3D imageExtent;
+  VkFormat imageFormat;
+};
+
+constexpr unsigned int FRAME_OVERLAP = 3;
+
+class VulkanEngine {
+public:
+  bool _isInitialized{false};
+  int _frameNumber{0};
+  bool stop_rendering{false};
+  VkExtent2D _windowExtent{1700, 900};
+
+  VkInstance _instance;
+  VkDebugUtilsMessengerEXT _debug_messenger;
+  VkPhysicalDevice _chosenGPU;
+  VkDevice _device;
+  VkSurfaceKHR _surface;
+
+  VkSwapchainKHR _swapchain;
+  VkFormat _swapchainImageFormat;
+
+  std::vector<VkImage> _swapchainImages;
+  std::vector<VkImageView> _swapchainImageViews;
+  VkExtent2D _swapchainExtent;
+  FrameData _frames[FRAME_OVERLAP];
+  FrameData &get_current_frame() {
+    return _frames[_frameNumber % (FRAME_OVERLAP)];
+  };
+
+  VkQueue _graphicsQueue;
+  uint32_t _graphicsQueueFamily;
+
+  struct SDL_Window *_window{nullptr};
+
+  static VulkanEngine &Get();
+
+  VmaAllocator _allocator;
+  DeletionQueue _mainDeletionQueue;
+
+  AllocatedImage _drawImage;
+  VkExtent2D _drawExtent;
+
+  VkDescriptorSet _drawImageDescriptors;
+  VkDescriptorSetLayout _drawImageDescriptorLayout;
+
+  VkPipeline _gradientPipeline;
+  VkPipelineLayout _gradientPipelineLayout;
+
+  VkFence _immFence;
+  VkCommandBuffer _immCommandBuffer;
+  VkCommandPool _immCommandPool;
+
+  DescriptorAllocator globalDescriptorAllocator;
+
+  std::vector<ComputeEffect> backgroundEffects;
+  int currentBackgroundEffect{0};
+
+  // initializes everything in the engine
+  void init();
+
+  // shuts down the engine
+  void cleanup();
+
+  // draw loop
+  void draw();
+
+  // run main loop
+  void run();
+
+  void immediate_submit(std::function<void(VkCommandBuffer cmd)> &&function);
+
+private:
+  void init_vulkan();
+  void init_swapchain();
+  void init_commands();
+  void init_sync_structures();
+
+  void create_swapchain(uint32_t width, uint32_t height);
+  void destroy_swapchain();
+
+  void draw_background(VkCommandBuffer cmd);
+
+  void init_descriptors();
+  void init_pipelines();
+  void init_background_pipelines();
+
+  void init_imgui();
+
+  void draw_imgui(VkCommandBuffer cmd, VkImageView targetImageView);
+};
